3e0715886d4b85e8c8a32d30a5903ff3388e9cfa
AmirHossein <amirht18@aut.ac.ir>
2019-07-07 16:50:47 +0430

    idk

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -14,7 +14,8 @@ class CompanyController extends Controller
      */
     public function index()
     {
-        //
+        $company = company::orderby('id', 'desc')->get();
+        return View('company.index',['company'=>company]);
     }
 
     /**
@@ -24,25 +25,36 @@ class CompanyController extends Controller
      */
     public function create()
     {
-        //
+        return view('company.create');
     }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        //
+
+        company::create([
+            'name'=>$request['name'],
+            'realm'=>$request['realm'],
+            'phone'=>$request['phone'],
+            'mobile'=>$request['mobile'],
+            'website'=>$request['web'],
+            'description'=>$request['desc'],
+
+        ]);
+
+        return redirect('company.create')->with('success', 'مطلب شما با موفقیت ثبت شد');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\company  $company
-     * @return \Illuminate\Http\Response
+     * @param  \App\company $company
+     * @return void
      */
     public function show(company $company)
     {
@@ -52,34 +64,47 @@ class CompanyController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\company  $company
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function edit(company $company)
+    public function edit($id)
     {
-        //
+
+        $company = company::find($id);
+        return View('company.edit', ['company' => $company,]);
     }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\company  $company
-     * @return \Illuminate\Http\Response
+     * @param  \Illuminate\Http\Request $request
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
      */
-    public function update(Request $request, company $company)
+    public function update(Request $request, $id)
     {
-        //
+        $company=company::find($id);
+        $company->update([
+            'name'=>$request['name'],
+            'realm'=>$request['realm'],
+            'phone'=>$request['phone'],
+            'mobile'=>$request['mobile'],
+            'website'=>$request['web'],
+            'description'=>$request['desc'],
+        ]);
+        return redirect('company.edit')->with('success', 'مطلب شما با موفقیت ویرایش شد');
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\company  $company
+     * @param $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy(company $company)
+    public function destroy($id)
     {
-        //
+        company::find($id)->delete();
+        return redirect('company.index')->with('success', 'مطلب شما با موفقیت حذف شد');
+
     }
 }
